34f9f40492743d899d489ad3cd0226f5db85cc46
lesiak <lesiak@outlook.com>
2018-02-27 10:02:14 +0100

    Renamed variable

--- JarReader.java
+++ JarReader.java
@@ -29,26 +29,26 @@ public class JarReader {
             throw new IllegalArgumentException("Callback must not be null");
         }
         String jarPath = jarUrl.getPath().substring(5);
-        String[] paths = jarPath.split("!");
-        try (FileInputStream jarFileInputStream = new FileInputStream(paths[0])) {
-            readStream(jarFileInputStream, 1, paths, callback);
+        String[] pathSegments = jarPath.split("!");
+        try (FileInputStream jarFileInputStream = new FileInputStream(pathSegments[0])) {
+            readStream(jarFileInputStream, 1, pathSegments, callback);
         }
     }
 
-    private static void readStream(InputStream jarFileInputStream, int pathSegmentToOpen, String[] paths, InputStreamCallback callback) throws IOException {
-        boolean isInsideInnermostJar = pathSegmentToOpen == paths.length - 1;
+    private static void readStream(InputStream jarFileInputStream, int pathSegmentToOpen, String[] pathSegments, InputStreamCallback callback) throws IOException {
+        boolean isInsideInnermostJar = pathSegmentToOpen == pathSegments.length - 1;
         ZipInputStream jarInputStream = new ZipInputStream(jarFileInputStream);
         ZipEntry jarEntry = null;
         while ((jarEntry = jarInputStream.getNextEntry()) != null) {
             String jarEntryName = "/" + jarEntry.getName();
-            if (!jarEntry.isDirectory() && jarEntryName.startsWith(paths[pathSegmentToOpen])) {
+            if (!jarEntry.isDirectory() && jarEntryName.startsWith(pathSegments[pathSegmentToOpen])) {
                 logger.debug("Entry {} with size {} and data size {}", jarEntryName, jarEntry.getSize(), jarEntry.getSize());
                 InputStream jarEntryStream = ByteStreams.limit(jarInputStream, jarEntry.getSize());
                 if (isInsideInnermostJar) {
                     callback.onFile(jarEntryName, jarFileInputStream);
 
                 } else {
-                    readStream(jarEntryStream, pathSegmentToOpen + 1, paths, callback);
+                    readStream(jarEntryStream, pathSegmentToOpen + 1, pathSegments, callback);
                 }
             }
         }
